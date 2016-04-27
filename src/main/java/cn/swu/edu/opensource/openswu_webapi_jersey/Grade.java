2310c72b936989456c93b63ad0103b4215867693
Xndxcsd <985857416@qq.com>
2016-04-27 12:04:19 +0800

    查成绩基本上能用了。

--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/Grade.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/Grade.java
@@ -15,33 +15,68 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 
-/**	
- *
+/**
  * @author 陈思定/chensiding@qq.com
- * 
  */
 @Path("grade")
 public class Grade {
 
-	SecurityFilter filter = new SecurityFilter();
-	//注入请求
-	@Context
-	ContainerRequest cr;
+    SecurityFilter filter = new SecurityFilter();
+    //注入请求
+    @Context
+    ContainerRequest cr;
 
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public GradeData getIt(SearchParam param) {
-		filter.filter(cr);
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public GradeData getIt(SearchParam param) {
+        filter.filter(cr);
+        //登录
 
-		Login login = new Login(param.getSwuID(), param.getPassword());
-		TotalInfo info = login.getBasicInfo();
-		SwuGrades grades = new SwuGrades(login.getClient());
-		Gson gson = new Gson();
-		info.setGrades(gson.fromJson(grades.lookup(param), GradeData.class));
 
-		return info.getGrades();
-	}
+        Login login = new Login(param.getSwuID(), param.getPassword());
+
+        //检查用户名和密码是否正确
+        if (login.getResponse().contains("用户不存在或密码错误")) {
+            return responseForError("用户不存在或密码错误");
+        }
+
+
+        //进入教务系统
+        TotalInfo info = login.getBasicInfo();
+
+        SwuGrades grades = new SwuGrades(login.getClient());
+        Gson gson = new Gson();
+        info.setGrades(gson.fromJson(grades.lookup(param), GradeData.class));
+
+        return info.getGrades();
+    }
+
+    /**
+     * 传入需要返回的错误信息字符串。将错误信息封装在返回的json中，格式不会发生变化，kcmc对应的值会变为错误信息，如：
+     * <pre>
+     * {
+     * "items": [
+     * {
+     * "kcmc": "用户不存在或密码错误"
+     * }
+     * ]
+     * }
+     * </pre>
+     *
+     * @param errorString
+     * @return json which contain error
+     */
+    private GradeData responseForError(String errorString) {
+        GradeData gradeData = new GradeData();
+        GradeData.Items errorItem = new GradeData.Items();
+        errorItem.setKcmc(errorString);
+        ArrayList<GradeData.Items> array = new ArrayList<>();
+        array.add(errorItem);
+        gradeData.setItems(array);
+        return gradeData;
+    }
 
 }
--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/GradesLookuper.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/GradesLookuper.java
@@ -1,13 +1,12 @@
 package cn.swu.edu.opensource.openswu_webapi_jersey.grade;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import cn.swu.edu.opensource.openswu_webapi_jersey.constant.Constant;
+import cn.swu.edu.opensource.openswu_webapi_jersey.utils.Client;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-import cn.swu.edu.opensource.openswu_webapi_jersey.constant.Constant;
-import cn.swu.edu.opensource.openswu_webapi_jersey.utils.Client;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 陈思定 on 2016/1/30.
@@ -34,8 +33,9 @@ public class GradesLookuper implements Lookup {
         nameValuePair.add(new BasicNameValuePair("queryModel.sortName", ""));
         nameValuePair.add(new BasicNameValuePair("queryModel.sortOrder", "asc"));
         nameValuePair.add(new BasicNameValuePair("time", "1"));
+        //提交的表单中， 学期的对应关系是这样的，3->1 12->2 16->3 所以需要转换一下
         nameValuePair.add(new BasicNameValuePair("xnm", "" + searchParam.getXnm()));
-        nameValuePair.add(new BasicNameValuePair("xqm", "" + searchParam.getXqm()));
+        nameValuePair.add(new BasicNameValuePair("xqm", "" + Constant.ALL_XQM[searchParam.getXqm()]));
 
         String response = this.client.doPost(Constant.urlGradeSearch + searchParam.getSwuID(), nameValuePair);
 
