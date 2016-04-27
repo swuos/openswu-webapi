317db762f0c8795f45b7fdd7771640492a91ecaf
Xndxcsd <985857416@qq.com>
2016-04-27 10:32:07 +0800

    eclipse不会玩。还是换回到IDEA.

--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/Grade.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/Grade.java
@@ -1,46 +1,47 @@
 package cn.swu.edu.opensource.openswu_webapi_jersey;
 
+import cn.swu.edu.opensource.openswu_webapi_jersey.auth.SecurityFilter;
+import cn.swu.edu.opensource.openswu_webapi_jersey.grade.GradeData;
+import cn.swu.edu.opensource.openswu_webapi_jersey.grade.SearchParam;
+import cn.swu.edu.opensource.openswu_webapi_jersey.grade.SwuGrades;
+import cn.swu.edu.opensource.openswu_webapi_jersey.grade.TotalInfo;
+import cn.swu.edu.opensource.openswu_webapi_jersey.login.Login;
+import com.google.gson.Gson;
+import org.glassfish.jersey.server.ContainerRequest;
+
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import cn.swu.edu.opensource.openswu_webapi_jersey.grade.SearchParam;
-import cn.swu.edu.opensource.openswu_webapi_jersey.grade.SwuGrades;
-import cn.swu.edu.opensource.openswu_webapi_jersey.login.Login;
 
 /**	
- * 
- * @author 陈思定
  *
+ * @author 陈思定/chensiding@qq.com
+ * 
  */
-
 @Path("grade")
 public class Grade {
 
-	@GET
-//	@Produces("text/plain")
+	SecurityFilter filter = new SecurityFilter();
+	//注入请求
+	@Context
+	ContainerRequest cr;
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getIt(){
-		SearchParam param = new SearchParam();
-		param.setSwuID("x");
-		param.setPassword("x");
-		param.setXnm(2015);
-		param.setXqm(1);
-		
-		Login login = new Login("x", "x");
-		login.getBasicInfo();
+	public GradeData getIt(SearchParam param) {
+		filter.filter(cr);
+
+		Login login = new Login(param.getSwuID(), param.getPassword());
+		TotalInfo info = login.getBasicInfo();
 		SwuGrades grades = new SwuGrades(login.getClient());
-		String res =grades.lookup(param);
-		System.out.println(res);
-		
-		return res;
+		Gson gson = new Gson();
+		info.setGrades(gson.fromJson(grades.lookup(param), GradeData.class));
+
+		return info.getGrades();
 	}
-	
+
 }
--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/SearchParam.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/SearchParam.java
@@ -1,7 +1,5 @@
 package cn.swu.edu.opensource.openswu_webapi_jersey.grade;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
 /**
  * Created by csd on 2016/3/2.
  */
@@ -12,28 +10,21 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 public class SearchParam {
 
-    private Integer xnm = null;
-    private Integer xqm = null;
+    public Integer xnm = null;
+    public Integer xqm = null;
 
-    private String swuID = null;
-    private String password = null;
+    public String swuID = null;
+    public String password = null;
 
     public SearchParam() {
 		// TODO Auto-generated constructor stub
 	}
-    public void setXnm(int xnm) {
+
+    public SearchParam(Integer xnm, Integer xqm, String swuID, String password) {
+
         this.xnm = xnm;
-    }
-
-    public void setXqm(int xqm) {
         this.xqm = xqm;
-    }
-
-    public void setSwuID(String swuID) {
         this.swuID = swuID;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
@@ -42,15 +33,32 @@ public class SearchParam {
         return xnm;
     }
 
+    public void setXnm(int xnm) {
+        this.xnm = xnm;
+    }
+
     public Integer getXqm() {
         return xqm;
+    }
+
+    public void setXqm(int xqm) {
+        this.xqm = xqm;
     }
 
     public String getSwuID() {
         return swuID;
     }
+
+    public void setSwuID(String swuID) {
+        this.swuID = swuID;
+    }
+
 	public String getPassword() {
 		return password;
 	}
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
 }
