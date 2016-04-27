deeb125bb9f6d657ca3de988bff64709f35f311b
Xndxcsd <985857416@qq.com>
2016-04-27 20:25:45 +0800

    成绩查询正式完工了。

--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/Grade.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/Grade.java
@@ -1,6 +1,7 @@
 package cn.swu.edu.opensource.openswu_webapi_jersey;
 
 import cn.swu.edu.opensource.openswu_webapi_jersey.auth.SecurityFilter;
+import cn.swu.edu.opensource.openswu_webapi_jersey.constant.Constant;
 import cn.swu.edu.opensource.openswu_webapi_jersey.grade.GradeData;
 import cn.swu.edu.opensource.openswu_webapi_jersey.grade.SearchParam;
 import cn.swu.edu.opensource.openswu_webapi_jersey.grade.SwuGrades;
@@ -18,33 +19,49 @@ import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 
 /**
- * @author 陈思定/chensiding@qq.com
+ * 查询成绩的类。由于直接由之前版本迁移而来，所以代码可能存在冗余，项目发展到一定时候会回过头来重新修改这个模块。
+ * 也希望任何人自行修改。
+ * @author chensiding/chensiding@qq.com
+ *
  */
 @Path("grade")
 public class Grade {
 
+    //利用该类进行基本认证
     SecurityFilter filter = new SecurityFilter();
-    //注入请求
+
+    //将请求注入
     @Context
     ContainerRequest cr;
 
+    /**
+     * 处理POST请求。
+     * MOXy会将接受到的json串自动转化java对象，也可将java对象转化为json串输出。
+     * 查询成绩中我们使用grade包下的GradeData作为POJO进行互相转化。
+     *
+     * @param param 符合规则的json
+     * @return 成功查询则返回查询结果，不成功则返回报错信息。
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public GradeData getIt(SearchParam param) {
         filter.filter(cr);
-        //登录
 
-
+        /*登录*/
         Login login = new Login(param.getSwuID(), param.getPassword());
 
-        //检查用户名和密码是否正确
+        /*检查用户名和密码是否正确*/
         if (login.getResponse().contains("用户不存在或密码错误")) {
             return responseForError("用户不存在或密码错误");
         }
+        /*检查学年和学期号是否合法*/
+        if (!(contain(new String[]{"1", "2", "3"}, param.getXqm())
+                && contain(Constant.ALL_XNM, param.getXnm()))) {
+            return responseForError("学年或学期号不合法");
+        }
 
-
-        //进入教务系统
+        /*进入教务系统*/
         TotalInfo info = login.getBasicInfo();
 
         SwuGrades grades = new SwuGrades(login.getClient());
@@ -55,19 +72,20 @@ public class Grade {
     }
 
     /**
+     *
      * 传入需要返回的错误信息字符串。将错误信息封装在返回的json中，格式不会发生变化，kcmc对应的值会变为错误信息，如：
      * <pre>
      * {
      * "items": [
      * {
-     * "kcmc": "用户不存在或密码错误"
+     * "kcmc": "errorString"
      * }
      * ]
      * }
      * </pre>
      *
      * @param errorString
-     * @return json which contain error
+     * @return 包含错误信息的json串
      */
     private GradeData responseForError(String errorString) {
         GradeData gradeData = new GradeData();
@@ -77,6 +95,22 @@ public class Grade {
         array.add(errorItem);
         gradeData.setItems(array);
         return gradeData;
+    }
+
+    /**
+     * 判断src中是否存在sub或sub是否为0（在接收参数的过程中，若post过来的某参数值为空。我们会将其默认赋值为0）
+     * 当传入的参数在合法值数组内或者为0时都认为是合法的，否则不合法。
+     *
+     * @param src 储存参数合法值的数组
+     * @param sub 某参数值
+     * @return 参数值是否合法
+     */
+    private Boolean contain(String[] src, Integer sub) {
+        for (String s :
+                src) {
+            if (sub == 0 || s.equals(sub.toString())) return true;
+        }
+        return false;
     }
 
 }
--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/GradesLookuper.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/GradesLookuper.java
@@ -33,8 +33,12 @@ public class GradesLookuper implements Lookup {
         nameValuePair.add(new BasicNameValuePair("queryModel.sortName", ""));
         nameValuePair.add(new BasicNameValuePair("queryModel.sortOrder", "asc"));
         nameValuePair.add(new BasicNameValuePair("time", "1"));
+        //提交过来的参数中，学年为空时，会被自动赋值为0，故作此判断，当用户提交的json参数中xnm为空时，这里也提交的参数为空。
+        if (searchParam.getXnm() == 0)
+            nameValuePair.add(new BasicNameValuePair("xnm", ""));
+        else
+            nameValuePair.add(new BasicNameValuePair("xnm", "" + searchParam.getXnm()));
         //提交的表单中， 学期的对应关系是这样的，3->1 12->2 16->3 所以需要转换一下
-        nameValuePair.add(new BasicNameValuePair("xnm", "" + searchParam.getXnm()));
         nameValuePair.add(new BasicNameValuePair("xqm", "" + Constant.ALL_XQM[searchParam.getXqm()]));
 
         String response = this.client.doPost(Constant.urlGradeSearch + searchParam.getSwuID(), nameValuePair);
