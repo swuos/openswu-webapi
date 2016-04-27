d9612a1ade30cc4167589ea2aa0fb6c59311341d
Xndxcsd <985857416@qq.com>
2016-04-27 21:19:02 +0800

    仓库弄错了。弄了好半天。

--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/MyResource.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/MyResource.java
@@ -1,21 +1,13 @@
 package cn.swu.edu.opensource.openswu_webapi_jersey;
 
-import java.io.IOException;
+import cn.swu.edu.opensource.openswu_webapi_jersey.auth.SecurityFilter;
+import org.glassfish.jersey.server.ContainerRequest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-
-import org.glassfish.grizzly.http.HttpHeader;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.HttpCodecFilter.HeaderParsingState;
-import org.glassfish.grizzly.http.util.Header;
-import org.glassfish.jersey.server.ContainerRequest;
-
-import cn.swu.edu.opensource.openswu_webapi_jersey.auth.SecurityFilter;
 
 /**
  * Root resource (exposed at "myresource" path)
--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/GradesLookuper.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/grade/GradesLookuper.java
@@ -1,13 +1,12 @@
 package cn.swu.edu.opensource.openswu_webapi_jersey.grade;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import cn.swu.edu.opensource.openswu_webapi_jersey.constant.Constant;
+import cn.swu.edu.opensource.openswu_webapi_jersey.utils.Client;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-import cn.swu.edu.opensource.openswu_webapi_jersey.constant.Constant;
-import cn.swu.edu.opensource.openswu_webapi_jersey.utils.Client;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 陈思定 on 2016/1/30.
@@ -34,8 +33,13 @@ public class GradesLookuper implements Lookup {
         nameValuePair.add(new BasicNameValuePair("queryModel.sortName", ""));
         nameValuePair.add(new BasicNameValuePair("queryModel.sortOrder", "asc"));
         nameValuePair.add(new BasicNameValuePair("time", "1"));
-        nameValuePair.add(new BasicNameValuePair("xnm", "" + searchParam.getXnm()));
-        nameValuePair.add(new BasicNameValuePair("xqm", "" + searchParam.getXqm()));
+        //提交过来的参数中，学年为空时，会被自动赋值为0，故作此判断，当用户提交的json参数中xnm为空时，这里也提交的参数为空。
+        if (searchParam.getXnm() == 0)
+            nameValuePair.add(new BasicNameValuePair("xnm", ""));
+        else
+            nameValuePair.add(new BasicNameValuePair("xnm", "" + searchParam.getXnm()));
+        //提交的表单中， 学期的对应关系是这样的，3->1 12->2 16->3 所以需要转换一下
+        nameValuePair.add(new BasicNameValuePair("xqm", "" + Constant.ALL_XQM[searchParam.getXqm()]));
 
         String response = this.client.doPost(Constant.urlGradeSearch + searchParam.getSwuID(), nameValuePair);
 
