f90b6a79affa021a5e32725d192383c357696fae
Xndxcsd <985857416@qq.com>
2016-09-02 10:00:35 +0800

    add simply validation module. check the request entity. we will add more validation method.

--- src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/filters/ParameterValidator.java
+++ src/main/java/cn/swu/edu/opensource/openswu_webapi_jersey/filters/ParameterValidator.java
@@ -11,51 +11,47 @@ import java.io.*;
  * Created by 西南大学开源协会 陈思定  on 2016/9/1.
  * <p>
  * Email : sidingchan@gmail.com
- *
+ * <p>
  * <p>在请求匹配到路径对应的方法前，对请求参数进行简单的有效性验证。</p>
  * <p>若接受到的json请求中，某项数据为空则显然是无效的</p>
  * <p>当请求无效的情况发生时，携带着相关信息返回</p>
  */
 
 @Provider
-public class ParameterValidator implements ContainerRequestFilter{
+public class ParameterValidator implements ContainerRequestFilter {
 
     StringBuffer entity;
 
     @Override
-    public void filter(ContainerRequestContext requestContext){
+    public void filter(ContainerRequestContext requestContext) {
         System.out.println("validator in");
-        if(requestContext.hasEntity()) {
-            // initialize entity.
-            setEntity(requestContext);
-
-            System.out.println(entity);
-
-
-        }else{
+        if (requestContext.hasEntity()) {
+            // TODO add the deeply parameters validate method.
+            // for example , parameter length and so on.
+        } else {
             throw new ParameterException("Parameters are required.");
         }
     }
 
 
-    public void setEntity(ContainerRequestContext requestContext){
-
-        this.entity = new StringBuffer();
-        String t;
-        BufferedReader bfReader = new BufferedReader(new InputStreamReader(requestContext.getEntityStream()));
-        try {
-            while((t=bfReader.readLine()) != null){
-                entity.append(t);
-            }
-        } catch (IOException e) {
-            throw new ParameterException("server problem.try again.");
-        } finally {
-            try {
-                bfReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    public void setEntity(ContainerRequestContext requestContext){
+//
+//        this.entity = new StringBuffer();
+//        String t;
+//        BufferedReader bfReader = new BufferedReader(new InputStreamReader(requestContext.getEntityStream()));
+//        try {
+//            while((t=bfReader.readLine()) != null){
+//                entity.append(t);
+//            }
+//        } catch (IOException e) {
+//            throw new ParameterException("server problem.try again.");
+//        } finally {
+//            try {
+//                bfReader.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
 }
